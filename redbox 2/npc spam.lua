b82874abb11b72ef77857ffda263a4a4bc479018
crosszay1 <104384163+crosszay1@users.noreply.github.com>
2022-09-24 14:56:32 -0700

    Create npc spam.lua

---
+++ redbox 2/npc spam.lua
@@ -0,0 +1,14 @@
+--NPC Spam--:
+
+game:GetService("RunService").Stepped:connect(function()
+
+for i,v in pairs(game.Players:GetChildren()) do
+   if v.Name ~= game.Players.LocalPlayer.Name and v.Character:FindFirstChild("Arrested").Value == false then
+local args = {
+   [1] = v.Character
+}
+
+game:GetService("ReplicatedStorage").Assets.Remotes.cuffsArrest:FireServer(unpack(args))
+end
+end
+end)
